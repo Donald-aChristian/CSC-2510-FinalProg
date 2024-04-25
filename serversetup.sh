1e369add20d2d2791955d70f3c8241483aeed9fa
Donald-aChristian <dgaoeser@gmail.com>
2024-04-25 16:16:44 -0500

    Initial commit only has comment block

---
+++ serversetup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Author: Donald Oeser
+# Date: 4/24/2024
+# Purpose: To take in tickets from a website that contains information on how the person wants to set up a server then set up the server
+
+
