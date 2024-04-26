d7ade94938abe0ab85df30b924659e4b29bcf72e
Donald-aChristian <dgaoeser@gmail.com>
2024-04-26 11:24:16 -0500

    Added install jq so rest of program will work, parameter intake, and curled the url

--- serversetup.sh
+++ serversetup.sh
@@ -3,4 +3,20 @@
 # Date: 4/24/2024
 # Purpose: To take in tickets from a website that contains information on how the person wants to set up a server then set up the server
 
+#installing jq so the rest of the program will work
+sudo apt-get install jq
+
+#getting information from website and storing it
+strCurledURL=$(curl "https://www.swollenhippo.com/ServiceNow/systems/devTickets.php" | jq)
+#debug statment
+echo ${strCurledURL}
+
+#parameters to hold the server ip address and the ticket ID to get information from
+strIP=$1
+strTicketID=$2
+#debug statments
+#echo ${strIP}
+#echo ${strTicketID}
+
+
 
